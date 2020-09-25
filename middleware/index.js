12dba3083502b8d031419da4be9c09331f8888bb
Sanskar Singh <18btc049@ietdavv.edu.in>
2020-09-25 15:22:09 +0000

    admin role

--- middleware/index.js
+++ middleware/index.js
@@ -12,7 +12,7 @@ middlewareObj.checkCampgroundOwnership = function(req, res, next) {
                res.redirect("back");
            }  else {
                // does user own the campground?
-            if(foundCampground.author.id.equals(req.user._id)) {
+            if(foundCampground.author.id.equals(req.user._id) || req.user.isAdmin) {
                 next();
             } else {
                 req.flash("error", "You don't have permission to do that");
@@ -27,13 +27,14 @@ middlewareObj.checkCampgroundOwnership = function(req, res, next) {
 }
 
 middlewareObj.checkCommentOwnership = function(req, res, next) {
+	console.log("YOU MADE IT");
  if(req.isAuthenticated()){
         Comment.findById(req.params.comment_id, function(err, foundComment){
            if(err){
                res.redirect("back");
            }  else {
                // does user own the comment?
-            if(foundComment.author.id.equals(req.user._id)) {
+            if(foundComment.author.id.equals(req.user._id) || req.user.isAdmin) {
                 next();
             } else {
                 req.flash("error", "You don't have permission to do that");
